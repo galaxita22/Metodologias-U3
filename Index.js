e4088ec1e36d178378f23b928fb2bb6a12fd0c7d
mario <mario.jorquera.11@gmail.com>
2024-07-01 17:08:04 -0400

    Phaser

---
+++ Index.js
@@ -0,0 +1,27 @@
+var config = {
+    type: Phaser.AUTO,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    parent: 'phaser-example',
+    backgroundColor: '#F5F5DC',
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+}
+
+var game = new Phaser.Game(config);
+
+
+function preload(){
+
+}
+
+function create(){
+    
+}
+
+function update(){
+
+}
